ee7616b53c5895595362041a4b55f87d1a813af9
Gemechis Chala Degefa <117309484+gladsonchala@users.noreply.github.com>
2023-05-21 00:52:49 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const hotelsRouter = require('./routes/hotels');
+const csasRouter = require('./routes/csas');
+const authRouter = require('./routes/auth');
+const adminRouter = require('./routes/admin');
+
+// Middleware
+app.use(bodyParser.json());
+
+// Routes
+app.use('/hotels', hotelsRouter);
+app.use('/csas', csasRouter);
+app.use('/auth', authRouter);
+app.use('/admin', adminRouter);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Internal Server Error');
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
